50a3ff5b7632e87e5ca207988f0e8a9941bcaf96
Christianp05 <chris@chrisLaptop.localdomain>
2026-02-12 16:13:27 +0100

    implemented template class

--- include/template_calculator.hpp
+++ include/template_calculator.hpp
@@ -14,13 +14,29 @@ public:
     // Implement this:
     T sum(T a, T b)
     {
-        return 0;
+
+        //log the result
+        std::stringstream ss;
+
+        ss << "taking the sum of: " << a << " and " << b << " which is " << a+b << std::endl;
+        std::string msg = ss.str();
+
+        this->logger->log(msg);
+
+        return a + b;
     }
 
     // Implement this:
     T multiply(T a, T b)
     {
-        return 0;
+        //log the result using a stringstream
+        std::stringstream ss;
+        
+        ss << "taking the product of: " << a << " and " << b << " which is " << a*b << std::endl;
+        std::string msg = ss.str();
+
+        this->logger->log(msg);
+        return a *b;
     }
 
 private:
